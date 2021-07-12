ab51392c9b75a8b682b63720ed4d80e870b39ee3
Matt Baxter <mbx33@hotmail.com>
2021-07-12 14:44:27 -0600

    third

--- calc.js
+++ calc.js
@@ -13,23 +13,31 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
+	num3 = Number(tokens[3]);
+
+	console.log('mathSymbol', mathSymbol);
+            console.log('num1', num1);
+            console.log('num2', num2);
 
 	if (mathSymbol === "+") {
-		console.log(num1 + num2);
+		console.log(num1 + num2 + num3);
 	}
 	else if (mathSymbol === "-") {
-		console.log(num1 - num2);
+		console.log(num1 - num2 - num3);
 	}
 	else if (mathSymbol === "/") {
 		console.log(num1 / num2);
 	}
 	else if (mathSymbol === "*") {
-		console.log(num1 * num2);
+		console.log(num1 * num2 * num3);
 	}
 	else if (mathSymbol === "Math.sqrt"){
 		console.log(Math.sqrt(num1));
 	}
-	
+	else if (mathSymbol === "**"){
+		console.log(num1 ** num2)
+	}
+
 		
 	
 
